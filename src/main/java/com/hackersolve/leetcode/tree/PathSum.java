6c99143f8fcbc5f13efcae98c195dc60dbce6f5f
Partha Protim Konwar <parthaprotimkonwar@gmail.com>
2016-12-15 13:49:46 +0530

    PathSolve

---
+++ src/main/java/com/hackersolve/leetcode/tree/PathSum.java
@@ -0,0 +1,58 @@
+package com.hackersolve.leetcode.tree;
+
+/**
+ * Created by pkonwar on 12/15/2016.
+ */
+public class PathSum {
+
+    /**
+     * Definition for binary tree
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+
+    //checks if there is a path from the the root to the leaf
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        if (root.left == null && root.right == null) {
+            if (sum == root.val) {
+                return true;
+            }
+        }
+        return calculateSum(root, sum, false);
+    }
+
+    //calculate the sum by traversing the tree
+    private boolean calculateSum(TreeNode root, int sum, boolean leaf) {
+
+        if (root == null && sum == 0 && leaf == true)
+            return true;
+        else if (root == null)
+            return false;
+
+        //updates the value of the sum for computing left and right sum
+        int updatedValue = sum - root.val;
+
+        if (root.left != null || root.right != null)
+            leaf = false;
+        else
+            leaf = true;
+        return calculateSum(root.left, updatedValue, leaf) || calculateSum(root.right, updatedValue, leaf);
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
