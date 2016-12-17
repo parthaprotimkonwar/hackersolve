f28b1b6647b9747fd4691f68340f849d6678ed45
Partha Protim Konwar <parthaprotimkonwar@gmail.com>
2016-12-17 08:37:02 +0530

    Symmetric Tree

---
+++ src/main/java/com/hackersolve/leetcode/tree/SymmetricTree.java
@@ -0,0 +1,43 @@
+package com.hackersolve.leetcode.tree;
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class SymmetricTree {
+
+    //check if the tree is symmetric or not
+    public boolean isSymmetric(TreeNode root) {
+
+        if (root == null)
+            return true;
+
+        if (root.left == null && root.right == null)
+            return true;
+
+        return mirror(root.left, root.right);
+    }
+
+    //check if the below two nodes of a tree are symmetric or not
+    boolean mirror(TreeNode firstTree, TreeNode secondTree) {
+
+        if (firstTree == null && secondTree == null) {
+            return true;
+        } else if (firstTree == null || secondTree == null) {
+            return false;
+        }
+        //checks if the value of the two nodes are same
+        if (firstTree.val != secondTree.val) {
+            return false;
+        }
+        //comparision for symmetry
+        //the left node of first tree and the right node of second tree
+        //the right node of first tree and left node of second tree.
+        return mirror(firstTree.left, secondTree.right) && mirror(firstTree.right, secondTree.left);
+    }
+}
