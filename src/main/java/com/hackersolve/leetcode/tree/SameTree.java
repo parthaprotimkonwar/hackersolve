693db654a24d821dd0f2c319616a6738860783cc
Partha Protim Konwar <parthaprotimkonwar@gmail.com>
2016-12-17 08:59:40 +0530

    SameTree

---
+++ src/main/java/com/hackersolve/leetcode/tree/SameTree.java
@@ -0,0 +1,17 @@
+package com.hackersolve.leetcode.tree;
+
+public class SameTree {
+
+    //check if both the trees are same
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+
+        //check if the both the left nodes and the right nodes are same
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
